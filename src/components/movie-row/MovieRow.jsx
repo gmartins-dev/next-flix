8d7ffbff0b3a12e50683bff5855218690a5118eb
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-04-17 09:55:27 -0300

    fixed error results.lenght

--- src/components/movie-row/MovieRow.jsx
+++ src/components/movie-row/MovieRow.jsx
@@ -13,7 +13,7 @@ export default function MovieRow ({title, items}) {
             <h2>{title}</h2>
 
             <div className="movieRow--listarea">
-                {items?.results.length > 0 && items?.results.map((item, key) => (
+                {items?.results?.length > 0 && items?.results.map((item, key) => (
                     
                     <img src= {`https://image.tmdb.org/t/p/w300${item.poster_path}`} />
                     
