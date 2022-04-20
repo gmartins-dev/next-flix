5928cb739de3c4009d748ee6cd8d035f3119c750
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-04-20 11:35:42 -0300

    tests

--- pages/index.jsx
+++ pages/index.jsx
@@ -108,7 +108,12 @@ export default function Home({list}) {
       </footer>
       
     
-     
+      {movieList.length <= 0 && /* verificação para o loading aparecer apenas antes do site carregar */
+       <div className="loading">
+        <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" />
+        </div>
+      }
+      
 
     </main>
   );
--- testeeeess.md
+++ testeeeess.md
@@ -1,5 +1,1 @@
- {movieList.length <= 0 && /* verificação para o loading aparecer apenas antes do site carregar */
-       <div className="loading">
-        <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" />
-        </div>
-      }
+asdasdas
