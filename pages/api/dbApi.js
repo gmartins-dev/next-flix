c2286531d418c7adb184caabbaf723e929c4fa0c
guilhermemm-dev <guilhermemm.dev@gmail.com>
2022-04-25 12:36:41 -0300

    small fixes

--- pages/api/dbApi.js
+++ pages/api/dbApi.js
@@ -15,43 +15,53 @@ export default {
         {
             slug: 'originals',
             title: 'Netflix Originals',
-            items: await basicFetch(`/discover/tv?with_network=213&api_key=${apiKey}`)
+            items: await basicFetch(`/discover/tv?api_key=${apiKey}&with_networks=213`)
         },
         {
             slug: 'trending',
             title: 'Recommended',
-            items: await basicFetch(`/discover/movie?sort_by=popularity.desc&api_key=${apiKey}`)
+            items: await basicFetch(`/trending/all/week?api_key=${apiKey}`)
         },
         //add top-rated and others categorys
         {
-            slug: 'theatres',
-            title: 'Movies in theatres',
-            items: await basicFetch(`/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22&sort_by=vote_average.desc&api_key=${apiKey}`)
+            slug: 'top-rated',
+            title: 'Top Rated Movies',
+            items: await basicFetch(`/movie/top_rated?api_key=${apiKey}`)
+        },
+        {
+            slug: 'top-rated-tv',
+            title: 'Top Rated Series',
+            items: await basicFetch(`/tv/top_rated?api_key=${apiKey}`)
         },
         {
             slug: 'action',
             title: 'Action',
-            items: await basicFetch(`/discover/movie?with_genres=28&api_key=${apiKey}`)
+            items: await basicFetch(`/discover/movie?api_key=${apiKey}&with_genres=28`)
         },
         {
             slug: 'comedy',
             title: 'Comedy',
-            items: await basicFetch(`/discover/movie?with_genres=35&api_key=${apiKey}`)
+            items: await basicFetch(`/discover/movie?api_key=${apiKey}&with_genres=35`)
+        },
+        {
+            slug: 'drama',
+            title: 'Drama',
+            items: await basicFetch(`/discover/movie?api_key=${apiKey}&with_genres=18`)
         },
         {
             slug: 'horror',
             title: 'Horror',
-            items: await basicFetch(`/discover/movie?with_genres=27&api_key=${apiKey}`)
+            items: await basicFetch(`/discover/movie?api_key=${apiKey}&with_genres=27`)
         },
         {
             slug: 'romance',
             title: 'Romance',
-            items: await basicFetch(`/discover/movie?with_genres=10749&api_key=${apiKey}`)
+            items: await basicFetch(`/discover/movie?api_key=${apiKey}&with_genres=10749`)
         },
         {
             slug: 'documentary',
             title: 'Documentary',
-            items: await basicFetch(`/discover/movie?with_genres=99&api_key=${apiKey}`)
+            items: await basicFetch(`/discover/movie?api_key=${apiKey}&with_genres=99`,)
         },
      ]
     },
